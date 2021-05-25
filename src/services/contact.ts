57845ea944961cfb50bfece1d4b1cb95f0b5fc4e
anyongjitiger <anyongjitiger@gmail.com>
2021-05-25 10:58:18 +0800

    contact

--- src/services/contact.ts
+++ src/services/contact.ts
@@ -3,18 +3,24 @@ import Contacts from 'react-native-contacts';
 import i18n from "@i18n";
 import { Contact, TContact } from "@types";
 
-const getAllContacts = () =>
-  PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_CONTACTS, {
-    title: i18n.t('Contacts'),
-    message: i18n.t('This app would like to view your contacts.'),
-    buttonPositive: i18n.t('Please accept bare mortal'),
-  })
+const requestReadPermission = () => {
+  if (Platform.OS === 'android') {
+    PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_CONTACTS, {
+      title: i18n.t('Contacts'),
+      message: i18n.t('This app would like to view your contacts.'),
+      buttonPositive: i18n.t('Please accept bare mortal'),
+    })
     .then(permissionAnswer => {
-      if (permissionAnswer === 'denied' && Platform.OS === 'android') {
+      if (permissionAnswer === 'denied') {
         return;
       }
       return Contacts.getAll();
-    })
+    });
+  }
+  return Contacts.getAll();
+}
+const getAllContacts = () =>
+  requestReadPermission()
     .then(contacts => {
       const mappedContacts: TContact[] =
         contacts
